e41e5ca1ede57ae6d08124ad7ba6573f7cb24e48
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-05 01:06:31 +0500

    Create a Const Object Called Profile to Store information and learn variable and data types

--- first.js
+++ first.js
@@ -1,7 +1,16 @@
-const Product = {
-    name: "junaid",
-    rating: 4,
-    offer:5,
-    price:120
+// const Product = {
+//     name: "junaid",
+//     rating: 4,
+//     offer:5,
+//     price:120
 
+// }
+
+
+const Profile = {
+    userName: "junaid",
+    isFollow : false,
+    Post :5,
+    followers :120,
+    following :120,
 }
