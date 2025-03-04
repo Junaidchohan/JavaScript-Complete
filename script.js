31f802b01341eec7771c1d144e2791d5a3827e91
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-05 02:42:49 +0500

    If else Statment

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-// Conditional Statments
+//If else Statments
 
+let age = 2;
 
-// If Statments
-
-let a = 12;
-
-if (a>18){
+if (age>18){
     console.log("You can vote!");
-}
-
-
-if (a<18){
+}else{
     console.log("You can't vote!");
 
 }
-
-
-let mode = "light";
-let color;
-
-if(mode=="dark"){
-    color = "black";
-}
-
-if(mode=="light"){
-    color = "white";
-}
-
-console.log(color);
-
-
-
-
