cd3988ad21a183999fdba68d387981ceeed89325
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-05 03:05:07 +0500

    called Simpller, Compect If Else

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let age = 25;
 
 age < 20 ? console.log( "Not Adults!") : console.log( "Adults!");
 
+
+
+//Normaly Ternary Oprater we  can use for the same purpose If Else but its use in the time of Simple conditions and also called Simpller, Compect If Else
+let age = 25;
+
+age < 20 ? console.log( "Not Adults!") : console.log( "Adults!"); 
+
