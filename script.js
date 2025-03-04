75f62a6c120196f98186c8093b335f2c1f7cc277
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-05 02:05:00 +0500

    Arthimatic Opraters

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Hello World")
+//Arthimatic Opraters
+
+let a = 5;
+let b = 2;
+
+console.log("a =", a, "b =", b);
+console.log("a+b =" ,a+b);
+console.log("a-b =" ,a-b);
+console.log("a*b =" ,a*b);
+console.log("a/b =" ,a/b);
+//Modulus
+console.log("a%b =" ,a%b);
+//Exponentiation
+console.log("a**b =" ,a**b);
+// Increment and Decrement Oprater
+
+
+//Post Increment
+// a = a + 1; = a++
+a++
+console.log(a);
+a
+
+//Post Decrement
+// a = a - 1; = a--
+a--
+console.log(a);
+
+
+
+
+
+//Post Increment
+// a = a + 1; = ++a
+++a
+console.log(a);
+
+//Post Decrement
+// a = a - 1; = --a
+--a
+console.log(a);
+
+
