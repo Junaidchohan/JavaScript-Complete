08c54dfbb6ec5799411334f03ef49deeeef43c0c
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-05 02:38:09 +0500

    If Statment

--- script.js
+++ script.js
@@ -1,63 +1,34 @@
-//Arthimatic Opraters
-
-// let a = 5;
-// let b = 2;
-
-// console.log("a =", a, "b =", b);
-// console.log("a+b =" ,a+b);
-// console.log("a-b =" ,a-b);
-// console.log("a*b =" ,a*b);
-// console.log("a/b =" ,a/b);
-//Modulus
-// console.log("a%b =" ,a%b);
-//Exponentiation
-// console.log("a**b =" ,a**b);
-// Increment and Decrement Oprater
+// Conditional Statments
 
 
-//Post Increment
-// a = a + 1; = a++
-// a++
-// console.log(a);
+// If Statments
+
+let a = 12;
+
+if (a>18){
+    console.log("You can vote!");
+}
 
 
-//Post Decrement
-// a = a - 1; = a--
-// a--
-// console.log(a);
+if (a<18){
+    console.log("You can't vote!");
+
+}
 
 
+let mode = "light";
+let color;
 
+if(mode=="dark"){
+    color = "black";
+}
 
+if(mode=="light"){
+    color = "white";
+}
 
-//Post Increment
-// a = a + 1; = ++a
-// ++a
-// console.log(a);
-
-//Post Decrement
-// a = a - 1; = --a
-// --a
-// console.log(a);
+console.log(color);
 
 
 
 
-
-
-
-//Asignment Oprater
-let a = 5;
-let b = 2;
-
-a += 5;  // a = a+5
-console.log(a);  //5+5=10
-
-a -= 4;  // a = a-5
-console.log(a);   //10-4=6
-
-a *= 5;  // a = a*5
-console.log(a);   //6*5=30
-
-a /= 5;  // a = a/5
-console.log(a);  //30/5=
