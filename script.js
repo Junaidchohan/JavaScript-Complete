0e5cf08844caff131f86f879f48c2d990312dd36
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-05 23:42:31 +0500

    while Loop

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 
-let i = 0;
+// let i = 0;
 
-for (let i = 1; i<=5; i++){
-    console.log("The value of count;" , i)
+// for (let i = 1; i<=5; i++){
+//     console.log("The value of count;" ,i)
+// }
+
+
+let i = 1;
+
+while(i <=5){
+    console.log("The value of count;" ,i);
+    i++;
 }
