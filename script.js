3e23b9ae04b78c9f6a4ae7be9efe14cc4fadb703
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-05 23:38:11 +0500

    For loop in JavaScript

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-//Task 2
 
 
-let marks = 75;
+let i = 0;
 
-let grade;
-
-if (marks >90){
-    grade = "A"
-}else if (marks >=80 && marks <= 90){
-    grade = "B"
-}else if (marks >=70 && marks <= 80){
-    grade = "C"
-}else if (marks >=60 && marks <= 70){
-    grade = "D"
-}else if (marks >=50 && marks <= 60){
-    grade = "D+"
+for (let i = 1; i<=5; i++){
+    console.log("The value of count;" , i)
 }
-else{
-    console.log(num, "Student is fail")
-
-}
-
-console.log("Acording to your marks, your grade was:", grade)
