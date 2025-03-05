adda8991d0876254b0e3e4b074f5613cbb8d791c
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-06 00:18:13 +0500

    Print the values of Object using For in loop

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
-// For In Loop its use for Objects and Arrays and print key but dont for Strings,
+// For In Loop its use for Objects and Arrays and print key and values but dont for Strings,
 
 
+
+
+// Student ={
+//     name : "Junaid",
+//     age   :25,
+//     cgpa : 3.5,
+// }
+
+// for (let key in Student){
+//     console.log("Key =", key);
+// }
 
 Student ={
     name : "Junaid",
@@ -12,3 +23,4 @@ Student ={
 for (let key in Student){
     console.log("Key =", key, "Value =", Student[key]);
 }
+
