5666620a845e9c2d5238f5dc5af62b5d2b76a939
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-08 02:55:27 +0500

    Arrays Indices

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
 
-
-//Arrays
-
+//Arrays Indices
 let Arrays = [12,21,23,34,45,56,43,35];
-console.log(Arrays);
-
+console.log(Arrays[0]);
+console.log(Arrays[0]=100);
+console.log(Arrays.length);
 
 
 
