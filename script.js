7ed361cc3357b01b1bfc833dde26dbeb472a9fab
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-08 03:07:22 +0500

    Looping over an Arrays

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
 
-//Arrays Indices
-let Arrays = [12,21,23,34,45,56,43,35];
-console.log(Arrays[0]);
-console.log(Arrays[0]=100);
-console.log(Arrays.length);
+//Looping over an Arrays
+let marks = [12,21,23,34,45,56,43,35];
+
+
+// for(let i = 0; marks < marks.length; i++ ){
+//     console.log(marks[i]);
+// }
 
 
 
+//For of loop in arrays
+
+for(let mark of marks){
+    console.log(mark);
+}
