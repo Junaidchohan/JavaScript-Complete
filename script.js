26877682eee66f66359e09c4cdf80441f17e7dec
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-08 05:11:49 +0500

    Basic Function

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+//Functions and Methods
 
+function myFunction(){
+    console.log("I am Flutter app and Web developer"),
+    console.log("I am Flutter app and Web developer"),
+    console.log("I am Flutter app and Web developer")
+}
 
-//Practice Question
-let company = ["Bloomberg","Microsoft", "Uber", "Google","IBM", "Netflex"]
-
-// company.shift();
-// console.log(company);
-// company.splice(2,1,"Ola")
-company.push("Amazone");
-console.log(company);
+myFunction();
