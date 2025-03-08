0d71db44d3345c852715c8c4935d8505ff356201
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-08 22:00:00 +0500

    Array filter Methode using for odd numbers

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Array filter Methode
 let num = [1,2,3,4,5,6];
 
- let evenArray = num.filter((num)=>{
-    return num%2==0;
+ let oddArray = num.filter((num)=>{
+    return num%2 !==0;
 })
 
-console.log(evenArray);
+console.log(oddArray);
