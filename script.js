d1ae019f6061090ce1d4aa8c41c7697f360683fc
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-08 21:49:22 +0500

    Map Methode

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 //print the  squre value for each number using forEach method
 let num = [1,2,3,4,5,6];
 
-let calcSqure = (num)=>{
-    console.log(num*num);
-}
 
-num.forEach(calcSqure);
+// num.map((num)=>{
+//     console.log(num*num);
+// })
+
+ let newArray = num.map((num)=>{
+    return num*num;
+})
+
+console.log(newArray);
