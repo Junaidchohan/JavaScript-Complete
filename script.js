4e564f201a7ec280022cf6972dbcd72a87b98382
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-08 21:44:12 +0500

    print the  squre value for each number using forEach method with using variableFunction

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //Practice Question
-
+//print the  squre value for each number using forEach method
 let num = [1,2,3,4,5,6];
 
-num.forEach((num)=>{
+let calcSqure = (num)=>{
     console.log(num*num);
-})
+}
+
+num.forEach(calcSqure);
