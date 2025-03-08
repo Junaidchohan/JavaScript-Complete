20920f3bd91e7afbecbd476bdc91dc47d1bd92a3
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-03-08 22:11:32 +0500

    Array reduce Methode for adding all valuse

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Array filter Methode
 let num = [1,2,3,4,5,6];
 
- let oddArray = num.filter((num)=>{
-    return num%2 !==0;
+ let output = num.reduce((pre, curr)=>{
+    return pre+curr;
 })
 
-console.log(oddArray);
+console.log(output);
