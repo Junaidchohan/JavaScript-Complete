e26b57c16a70b7aacfc2998d484ad3fc94e147fe
Junaidchohane <junaidchohanedu@gmail.com>
2025-03-20 08:06:59 +0500

    Learn all concept related to  DOM

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//  let heading = document.getElementById("heading"); 
+
+//  console.dir(heading);
+
+//  let headings = document.getElementsByClassName("heading-class");
+//  console.log(headings);
+
+let parah = document.querySelector("p");
+console.dir(parah);
+
+
+let parahs = document.querySelectorAll(". heading-class");
+console.dir(parahs);
